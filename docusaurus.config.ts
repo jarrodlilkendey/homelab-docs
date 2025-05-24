a89f3bdf240df0ea955d7a09dd49aa10b9d1202b
Jarrod Lilkendey <programmablewealth@gmail.com>
2025-05-24 13:48:12 +1000

    update npm packages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "programmablewealth", // Usually your GitHub org/user name.
+  organizationName: "jarrodlilkendey", // Usually your GitHub org/user name.
   projectName: "homelab-docs", // Usually your repo name.
 
   onBrokenLinks: "throw",
